fbe0c700b196be5bbd5e8e4442cc96cab15bcfc6
muhammadasadraza69@gmail.com <muhammadasadraza69@gmail.com>
2024-01-07 20:22:57 +0500

    update

--- home.php
+++ home.php
@@ -1,88 +1,147 @@
-<?php require "header.php" ?>
-<?php require "navbar.php" ?>
+<!DOCTYPE html>
 <?php
-
+require "connection.php";
 ?>
-<div class="layout-wrapper layout-content-navbar layout-without-menu">
-    <div class="layout-container">
-        <!-- Layout container -->
-        <div class="layout-page">
+<html lang="en" class="light-style layout-wide customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="../assets/" data-template="vertical-menu-template-free">
 
-            <!-- Content wrapper -->
-            <div class="content-wrapper">
-                <!-- Content -->
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-                <div class="container-xxl flex-grow-1 container-p-y">
-                    <!-- Layout Demo -->
-                    <div class="layout-demo-wrapper d-flex flex-row-reverse p-3">
-                        <div></div>
-                    </div>
+    <title>Login Basic - Pages | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
 
-                    <!-- Bootstrap Table with Header - Dark -->
-                    <div id="authenti-inner"></div>
-                    <div class="card">
-                        <h5 class="card-header" style="background-color: #696cff; color: white;">Category ListView</h5>
+    <meta name="description" content="" />
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
 
-                        <div class="table-responsive text-nowrap">
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th>Sno</th>
-                                        <th><i class='bx bx-category'></i>Catergory List</th>
-                                        <th><i class='bx bx-detail'></i>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody class="table-border-bottom-0">
-                                    <?php
-                                    // $query = "SELECT * FROM user ORDER BY CASE WHEN role = 'Admin' THEN 0 ELSE 1 END, pk DESC";
-                                    $query = "SELECT * FROM category ORDER BY pk DESC";
-                                    $result = mysqli_query($conn, $query);
-                                    $index = 1;
-                                    ?>
-                                    <?php while ($row = mysqli_fetch_array($result)) {
-                                    ?>
-                                        <tr>
-                                            <td>
-                                                <!-- <i class="bx bxl-angular bx-sm text-danger me-3"></i> -->
-                                                <span class="fw-medium"><?php echo $index++ ?></span>
-                                            </td>
-                                            <td>
-                                                <!-- <i class="bx bxl-angular bx-sm text-danger me-3"></i> -->
-                                                <span class="fw-medium"><?php echo $row['name'] ?></span>
-                                            </td>
-                                            <td>
-                                                <div class="dropdown">
-                                                    <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                                                        <i class="bx bx-dots-vertical-rounded"></i>
-                                                    </button>
-                                                    <div class="dropdown-menu">
-                                                        <a class="dropdown-item" href="attempt_quiz_page.php?cat_pk=<?php echo $row['pk']; ?>"><i class='bx bxs-detail'></i>View Question</a>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    <?php
-                                    }
-                                    ?>
-                                </tbody>
-                            </table>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+
+    <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="../assets/css/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="../assets/vendor/css/pages/page-auth.css" />
+
+    <!-- Helpers -->
+    <script src="../assets/vendor/js/helpers.js"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="../assets/js/config.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+    <div class="container-xxl">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+                <!-- Register Card -->
+                <div class="card">
+                    <div class="card-body">
+                        <!-- Logo -->
+                        <div class="app-brand justify-content-center">
+                            <a href="index.html" class="app-brand-link gap-2">
+                                <span class="app-brand-logo demo">
+                                    <img src="https://www.domain.de/_img/logos/head_normal.png" alt="logo.png" class="img-fluid" style="width: 200px; height: auto;">
+                                    </svg>
+                                </span>
+                                <!-- <span class="app-brand-text demo text-body fw-bold">Domain.De</span> -->
+                            </a>
                         </div>
+                        <!-- /Logo -->
+                        <div id="authenti-inner"></div>
+
+                        <h4 class="mb-2">Welcome to Domain.de 🚀</h4>
+                        <p class="mb-4">Sign up Here</p>
+
+                        <form id="formAuthentication" class="mb-3" action="" method="POST">
+                            <div class="mb-3">
+                                <label for="fullname" class="form-label">Full name</label>
+                                <input type="text" class="form-control" id="fullname" name="fullname" placeholder="Enter your Full name" autofocus required />
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="text" class="form-control" id="email" name="email" placeholder="Enter your email" required />
+                            </div>
+                            <div class="mb-3 form-password-toggle">
+                                <label class="form-label" for="password">Password</label>
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="password" class="form-control" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" required />
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                </div>
+                            </div>
+
+                            <button class="btn btn-primary d-grid w-100 " name="addUser">Sign up</button>
+                        </form>
+
+                        <p class="text-center">
+                            <span>Already have an account?</span>
+                            <a href="index.php">
+                                <span>Sign in instead</span>
+                            </a>
+                        </p>
                     </div>
-
                 </div>
-
+                <!-- Register Card -->
             </div>
-            <!-- Content wrapper -->
         </div>
-        <!-- / Layout page -->
     </div>
-</div>
-<script>
-    $(document).ready(function() {
-        console.log("Ready");
-       
-       
-    });
-</script>
-<?php require "footer.php" ?>
+    <script>
+        $(document).ready(function() {
+            console.log("Ready");
+
+
+        });
+    </script>
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <section id="component-footer">
+
+        <footer class="footer bg-light">
+            <div class="container-fluid d-flex flex-md-row flex-column justify-content-between align-items-md-center gap-1 container-p-x py-3">
+                <div class="mb-2 mb-md-0">
+                    ©
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script>, All Copyrights reserved
+                </div>
+                <div>
+                    <!-- <a href="logout.php" class="btn btn-sm btn-outline-danger"><i class="bx bx-log-out-circle me-1"></i>Logout</a> -->
+                </div>
+            </div>
+        </footer>
+    </section>
+    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../assets/vendor/libs/popper/popper.js"></script>
+    <script src="../assets/vendor/js/bootstrap.js"></script>
+    <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="../assets/vendor/js/menu.js"></script>
+
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+    <!-- Main JS -->
+    <script src="../assets/js/main.js"></script>
+
+    <!-- Page JS -->
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <!-- <script async defer src="https://buttons.github.io/buttons.js"></script> -->
+</body>
+
+</html>
