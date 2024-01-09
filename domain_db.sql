053c280885fa555bec484b59a8ee77c903872ef6
muhammadasadraza69@gmail.com <muhammadasadraza69@gmail.com>
2024-01-10 02:31:56 +0500

    update

--- domain_db.sql
+++ domain_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 07, 2024 at 08:14 PM
+-- Generation Time: Jan 09, 2024 at 10:31 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -86,7 +86,11 @@ INSERT INTO `answer` (`pk`, `answer`, `question_id`, `status`) VALUES
 (53, 'Answer-1112', 19, 0),
 (54, 'Answer-2223', 19, 0),
 (55, 'Answer-3334', 19, 0),
-(56, 'Answer-4445', 19, 1);
+(56, 'Answer-4445', 19, 1),
+(57, 'dsasad', 20, 1),
+(58, 'sdaads', 20, 0),
+(59, 'dasasd', 20, 0),
+(60, 'dasdas', 20, 0);
 
 -- --------------------------------------------------------
 
@@ -96,17 +100,19 @@ INSERT INTO `answer` (`pk`, `answer`, `question_id`, `status`) VALUES
 
 CREATE TABLE `category` (
   `pk` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
+  `name` varchar(255) NOT NULL,
+  `visible` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `category`
 --
 
-INSERT INTO `category` (`pk`, `name`) VALUES
-(2, 'Searchberg'),
-(3, 'Jumpto1'),
-(5, 'bookwritingcube');
+INSERT INTO `category` (`pk`, `name`, `visible`) VALUES
+(2, 'searchberg', 1),
+(3, 'Jumpto1', 0),
+(5, 'bookwritingcube', 1),
+(8, 'sa', 1);
 
 -- --------------------------------------------------------
 
@@ -119,6 +125,7 @@ CREATE TABLE `category_question` (
   `question` varchar(255) NOT NULL,
   `category_id` int(11) NOT NULL,
   `description` text DEFAULT NULL,
+  `visible` int(11) NOT NULL,
   `true_child_question` int(11) DEFAULT NULL,
   `false_child_question` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -127,19 +134,20 @@ CREATE TABLE `category_question` (
 -- Dumping data for table `category_question`
 --
 
-INSERT INTO `category_question` (`pk`, `question`, `category_id`, `description`, `true_child_question`, `false_child_question`) VALUES
-(1, 'What is Your Name?', 5, NULL, 0, 0),
-(2, 'What is Your Height?', 5, NULL, 0, 0),
-(3, 'What is Your Age?', 5, NULL, 0, 0),
-(4, 'Question500?', 5, NULL, 0, 0),
-(5, 'Question501?', 5, NULL, 0, 0),
-(6, 'Question502?', 5, NULL, 0, 0),
-(7, 'Question503?', 5, NULL, 0, 0),
-(10, 'What is Your Weight?', 5, NULL, 0, 0),
-(16, 'This is the Question #1', 5, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae fuga quam aliquam consequuntur ipsa ab quaerat ut, assumenda non atque. Sunt nesciunt dignissimos amet est, ut eos dolorem vitae placeat.', 5, 4),
-(17, 'This is the Question #2', 5, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam obcaecati quis assumenda placeat soluta, enim possimus eligendi? Debitis similique harum, temporibus dignissimos autem quidem ut eos amet sapiente delectus suscipit.', 5, 4),
-(18, 'This is the Question #3', 5, '1Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi accusantium iusto eveniet voluptas, ab quas labore delectus, molestiae deleniti laborum temporibus nobis repudiandae obcaecati cum iure, maxime quasi aut ex!', 3, 10),
-(19, 'This is the Question #4', 5, 'LOEAIKJAIJADIOJADOIJADO00', 0, 0);
+INSERT INTO `category_question` (`pk`, `question`, `category_id`, `description`, `visible`, `true_child_question`, `false_child_question`) VALUES
+(1, 'What is Your Name?', 5, NULL, 0, 0, 0),
+(2, 'What is Your Height?', 5, NULL, 0, 0, 0),
+(3, 'What is Your Age?', 5, NULL, 0, 0, 0),
+(4, 'Question500?', 5, NULL, 0, 0, 0),
+(5, 'Question501?', 5, NULL, 0, 0, 0),
+(6, 'Question502?', 5, NULL, 0, 0, 0),
+(7, 'Question503?', 5, NULL, 0, 0, 0),
+(10, 'What is Your Weight?', 5, NULL, 0, 0, 0),
+(16, 'This is the Question #1', 5, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae fuga quam aliquam consequuntur ipsa ab quaerat ut, assumenda non atque. Sunt nesciunt dignissimos amet est, ut eos dolorem vitae placeat.', 0, 5, 4),
+(17, 'This is the Question #2', 5, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam obcaecati quis assumenda placeat soluta, enim possimus eligendi? Debitis similique harum, temporibus dignissimos autem quidem ut eos amet sapiente delectus suscipit.', 1, 5, 4),
+(18, 'This is the Question #3', 5, '1Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi accusantium iusto eveniet voluptas, ab quas labore delectus, molestiae deleniti laborum temporibus nobis repudiandae obcaecati cum iure, maxime quasi aut ex!', 1, 3, 10),
+(19, 'This is the Question #4', 5, 'LOEAIKJAIJADIOJADOIJADO00', 1, 0, 0),
+(20, 'dsa', 9, 'ads', 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -163,7 +171,8 @@ INSERT INTO `user` (`pk`, `full_name`, `email`, `password`, `role`) VALUES
 (1, 'Alpha Admin', 'admin@gmail.com', '$2y$10$OtjAG9M1yuVbVXUJsKdZu.aYZI/XsMqtuoJu3XzRdLT2HYsjz74va', 'admin'),
 (2, 'User', 'user@gmail.com', '$2y$10$acEl0h5vwLDcFDRtp.t7e.QPhmhPc5jCo/gWNu/PPIavKIi9dR9wa', 'admin'),
 (3, 'Beta Admin', 'admin2@gmail.com', '$2y$10$OtjAG9M1yuVbVXUJsKdZu.aYZI/XsMqtuoJu3XzRdLT2HYsjz74va', 'admin'),
-(5, 'asad', 'sxadsa@gmail.net', '$2y$10$wBbb.RSBVsgMsYdWEcthTOpp59KszIQn2w02j3jZLPQ1.XsuoXM22', 'admin');
+(5, 'asad', 'sxadsa@gmail.net', '$2y$10$wBbb.RSBVsgMsYdWEcthTOpp59KszIQn2w02j3jZLPQ1.XsuoXM22', 'admin'),
+(7, 'asad', 'ninec15192@pursip.com', '$2y$10$BqgH/UWTtBzuuv.cmVlOKOUmMnHvelhuVKY1uARAf/5lPTnWNjlCq', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -202,25 +211,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `answer`
 --
 ALTER TABLE `answer`
-  MODIFY `pk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `pk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `pk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `pk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `category_question`
 --
 ALTER TABLE `category_question`
-  MODIFY `pk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `pk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `pk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `pk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- erstekategorie.php
+++ erstekategorie.php
@@ -48,37 +48,11 @@ require "connection.php";
 <body>
 
     <div class="container-xxl">
-        <div class="modal fade" id="deleteQuestionModal" tabindex="-1" aria-hidden="true">
-            <div class="modal-dialog" role="document" data-backdrop="static" data-keyboard="false">
-                <form action="" method="post">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="modalCenterTitle">Oops Invalid Category</h5>
-                            <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
-                        </div>
-                        <div class="modal-body">
-                            <div class="row">
-                                <div class="col mb-3">
-                                    <label id="deleteQuestionTitle" for="deleteQuestionTitle" class="form-label">No Category Found</label>
-                                    <!-- <input type="hidden" name="delete_category_pk" id="delete_category_pk" /> -->
-                                </div>
-                            </div>
-                        </div>
-                        <!-- <div class="modal-footer">
-                            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                                Close
-                            </button>
-                            <button type="submit" class="btn btn-primary" name="deleteQuestionbtn">Confirm</button>
-                        </div> -->
-                    </div>
-                </form>
-            </div>
-        </div>
         <div class="authentication-wrapper authentication-basic container-p-y">
             <div class="authentication-inner">
                 <!-- Register Card -->
-                <div class="card">
-                    <div class="card-body">
+                <div class="card" style="width: 35rem; height : 30rem">
+                    <div class="card-body d-flex flex-column align-items-center justify-content-center">
                         <!-- Logo -->
                         <div class="app-brand justify-content-center">
                             <a href="index.html" class="app-brand-link gap-2">
@@ -91,40 +65,39 @@ require "connection.php";
                         </div>
                         <!-- /Logo -->
 
-
-                        <h3 class="mb-2" id="categoryTitleHead">Category Name Will be Here</h3>
+                        <h1 class="mb-2" id="categoryTitleHead"></h1>
                         <div id="authenti-inner" class="py-2"></div>
-                        <form id="formAuthentication" class="mb-3" action="" method="POST">
-                            <h5 class="mb-2" id="categoryQuestion">Category Quewstion Will be Here</h5>
+                        <form id="formAuthentication" class="mb-3  d-flex flex-column align-items-center justify-content-center" action="" method="POST">
+                            <h5 class="mb-2" id="categoryQuestion"></h5>
                             <p class="mb-4"></p>
                             <div class="row  d-flex align-items-center">
                                 <div class="col-1 mb-3 parent">
                                     <input name="correctAnswer" class="form-check-input" type="radio" value="0" id="answerradio1">
                                 </div>
                                 <div class="col-11 mb-3 parent">
-                                    <h5 class="mb-2" id="answer1" name="answer1">First Correct answer</h5>
+                                    <h5 class="mb-2" id="answer1" name="answer1"></h5>
                                 </div>
                                 <div class="col-1 mb-3">
                                     <input name="correctAnswer" class="form-check-input" type="radio" value="0" id="answerradio2">
                                 </div>
                                 <div class="col-11 mb-3 parent">
-                                    <h5 class="mb-2" id="answer2" name="answer2">Second Correct answer</h5>
+                                    <h5 class="mb-2" id="answer2" name="answer2"></h5>
                                 </div>
                                 <div class="col-1 mb-3 parent">
                                     <input name="correctAnswer" class="form-check-input" type="radio" value="0" id="answerradio3">
                                 </div>
                                 <div class="col-11 mb-3 parent">
-                                    <h5 class="mb-2" id="answer3" name="answer3">Third Correct answer</h5>
+                                    <h5 class="mb-2" id="answer3" name="answer3"></h5>
                                 </div>
                                 <div class="col-1 mb-3 parent">
                                     <input name="correctAnswer" class="form-check-input" type="radio" value="0" id="answerradio4">
                                 </div>
                                 <div class="col-11 mb-3 parent">
-                                    <h5 class="mb-2" id="answer4" name="answer4">Fourth Correct answer</h5>
+                                    <h5 class="mb-2" id="answer4" name="answer4"></h5>
                                 </div>
 
                             </div>
-                            <div class=" d-flex align-items-center justify-content-around" id="jokerBtnDiv">
+                            <div class="mt-2 d-flex align-items-center justify-content-around" id="jokerBtnDiv">
                                 <button type="button" class="btn btn-primary d-grid w-25 jokerBtnDiv" name="joker1" id="joker1">Joker1</button>
                                 <button type="button" class="btn btn-primary d-grid w-25 jokerBtnDiv" name="joker2" id="joker2">Joker2</button>
                                 <button type="button" class="btn btn-primary d-grid w-25 jokerBtnDiv" name="joker3" id="joker3">Joker3</button>
@@ -142,58 +115,134 @@ require "connection.php";
             <?php
             $urlParams = isset($_GET['args']) ? $_GET['args'] : '';
             $urlParamsDecoded = urldecode($urlParams);
-            // echo "console.log('URL Params:', '$urlParamsDecoded');";
-
             $urlParamsDecoded = mysqli_real_escape_string($conn, $urlParamsDecoded);
-            $query = "SELECT * FROM category WHERE name = '$urlParamsDecoded'";
-            $result = mysqli_query($conn, $query);
 
-            if ($result) {
-                $category_obj = mysqli_fetch_assoc($result);
-                // echo "console.log('Category found:',", json_encode($category_obj), ");";
+            $query = "SELECT * FROM category WHERE name = '$urlParamsDecoded' and visible = 1";
+            $result1 = mysqli_query($conn, $query);
+            $count = mysqli_fetch_array($result1);
+            if ($count > 0) {
+                $category_obj = $count;
                 echo "$('#categoryTitleHead').text('" . $category_obj['name'] . "').css('text-transform', 'uppercase');";
 
                 $category_pk = $category_obj['pk'];
-                $query = "SELECT * FROM category_question WHERE category_id = $category_pk";
-                $result = mysqli_query($conn, $query);
-                $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-                $numRows = count($rows);
-                if ($numRows > 0) {
-                    $randomQuestionObj = $rows[rand(0, $numRows - 1)];
-                    echo "$('#categoryQuestion').text('" . $randomQuestionObj['question'] . "').css('text-transform', 'uppercase');";
-                    // echo "console.log('Category Question found:',", json_encode($randomQuestionObj), ");";
-                    $question_pk = $randomQuestionObj['pk'];
-                    $query = "SELECT * FROM answer WHERE question_id = $question_pk";
-                    $result = mysqli_query($conn, $query);
-                    $index = 1; // Make sure $index is initialized
+                $query = "SELECT * FROM category_question WHERE category_id = $category_pk AND visible = 1";
+                $result2 = mysqli_query($conn, $query);
 
-                    while ($rows = mysqli_fetch_array($result)) {
-                        $id = "#answer" . $index;
-                        // echo "console.log('Category Answer found:',", json_encode($id), ");";
-                        echo "$('" . $id . "').text('" . strtoupper($rows['answer']) . "');";
+                if ($result2) {
+                    $rows = mysqli_fetch_all($result2, MYSQLI_ASSOC);
+                    $numRows = count($rows);
 
-                        // Fix: Wrap the desiredRadioId in quotes
-                        echo "var desiredRadioId = 'answerradio" . $index . "';";
-                        // echo "console.log('desiredRadioId:' +desiredRadioId);";
-                        echo "if (" . $rows['status'] . " == 1) {";
-                        // Fix: Add missing quotes around #desiredRadioId
-                        echo "    $('#' + desiredRadioId).prop('value', 1);";
-                        echo "} else {";
-                        echo "    $('#' + desiredRadioId).prop('value', 0);";
-                        echo "}";
-                        $index++;
+                    if ($numRows > 0) {
+                        $randomQuestionObj = $rows[rand(0, $numRows - 1)];
+                        echo "$('#categoryQuestion').text('" . $randomQuestionObj['question'] . "').css('text-transform', 'uppercase');";
+
+                        $question_pk = $randomQuestionObj['pk'];
+                        $query = "SELECT * FROM answer WHERE question_id = $question_pk";
+                        $result3 = mysqli_query($conn, $query);
+
+                        $index = 1;
+                        while ($rows = mysqli_fetch_array($result3)) {
+                            $id = "#answer" . $index;
+                            echo "$('" . $id . "').text('" . strtoupper($rows['answer']) . "');";
+
+                            // Fix: Wrap the desiredRadioId in quotes
+                            $desiredRadioId = 'answerradio' . $index;
+                            echo "var desiredRadioId = '" . $desiredRadioId . "';";
+                            echo "if (" . $rows['status'] . " == 1) {";
+                            echo "    $('#' + desiredRadioId).prop('value', 1);";
+                            echo "} else {";
+                            echo "    $('#' + desiredRadioId).prop('value', 0);";
+                            echo "}";
+                            $index++;
+                        }
+                    } else {
+                        $randomQuestionObj = "No Question Found";
+                        echo "$('#categoryQuestion').text('" . $randomQuestionObj . "').css('text-transform', 'uppercase');";
+                        // $index = 1;
+                        // while ($index <= 4) {
+                        //     $id = "#answer" . $index;
+                        //     echo "$('" . $id . "').text('" . strtoupper("Option Doesnt Exist") . "');";
+
+                        //     // Fix: Wrap the desiredRadioId in quotes
+                        //     $desiredRadioId = 'answerradio' . $index;
+                        //     echo "var desiredRadioId = '" . $desiredRadioId . "';";
+                        //     echo "if (" . 0 . " == 1) {";
+                        //     echo "    $('#' + desiredRadioId).prop('value', 1);";
+                        //     echo "} else {";
+                        //     echo "    $('#' + desiredRadioId).prop('value', 0);";
+                        //     echo "}";
+                        //     $index++;
+                        // }
+                        echo "$('#jokerBtnDiv').css('display', 'none');";
+                        echo "$('#joker1').attr('style', 'display: none !important');";
+                        echo "$('#joker2').attr('style', 'display: none !important');";
+                        echo "$('#joker3').attr('style', 'display: none !important');";
+
+                        echo "$('input[name=\'correctAnswer\']').hide();";
                     }
-                    // answer1
-                    // echo "console.log('Category Answer found:',", json_encode($rows), ");";
-
                 } else {
-                    echo "No rows found.";
+                    $randomQuestionObj = "No Question Found";
+                    echo "$('#categoryQuestion').text('" . $randomQuestionObj . "').css('text-transform', 'uppercase');";
+                    echo  "$('.jokerBtnDiv').prop('disabled', true);";
+
+                    // $index = 1;
+                    // while ($index <= 4) {
+                    //     $id = "#answer" . $index;
+                    //     echo "$('" . $id . "').text('" . strtoupper("Answer is not avaliable") . "');";
+
+                    //     // Fix: Wrap the desiredRadioId in quotes
+                    //     $desiredRadioId = 'answerradio' . $index;
+                    //     echo "var desiredRadioId = '" . $desiredRadioId . "';";
+                    //     echo "if (" . 0 . " == 1) {";
+                    //     echo "    $('#' + desiredRadioId).prop('value', 1);";
+                    //     echo "} else {";
+                    //     echo "    $('#' + desiredRadioId).prop('value', 0);";
+                    //     echo "}";
+                    //     $index++;
+                    // }
+
+
+                    echo "$('#jokerBtnDiv').css('display', 'none');";
+                    echo "$('#joker1').attr('style', 'display: none !important');";
+                    echo "$('#joker2').attr('style', 'display: none !important');";
+                    echo "$('#joker3').attr('style', 'display: none !important');";
+
+                    echo "$('input[name=\'correctAnswer\']').hide();";
                 }
             } else {
-                // echo "console.log('Error fetching category.');";
-                echo "$('#deleteQuestionModal').modal('show');";
+                $category_obj = "Category Doesn\'t Exist";
+                echo "$('#categoryTitleHead').text('" . $category_obj . "').css('text-transform', 'uppercase');";
+
+                $randomQuestionObj = "No Question Found";
+                echo "$('#categoryQuestion').text('" . $randomQuestionObj . "').css('text-transform', 'uppercase');";
+
+                // $index = 1;
+                // while ($index <= 4) {
+                //     $id = "#answer" . $index;
+                //     echo "$('" . $id . "').text('" . strtoupper("Answer is not avaliable") . "');";
+
+                //     // Fix: Wrap the desiredRadioId in quotes
+                //     $desiredRadioId = 'answerradio' . $index;
+                //     echo "var desiredRadioId = '" . $desiredRadioId . "';";
+                //     echo "if (" . 0 . " == 1) {";
+                //     echo "    $('#' + desiredRadioId).prop('value', 1);";
+                //     echo "} else {";
+                //     echo "    $('#' + desiredRadioId).prop('value', 0);";
+                //     echo "}";
+                //     $index++;
+                // }
+
+                echo "$('.jokerBtnDiv').attr('style', 'display: none !important');";
+                echo "$('#joker1').attr('style', 'display: none !important');";
+                echo "$('#joker2').attr('style', 'display: none !important');";
+                echo "$('#joker3').attr('style', 'display: none !important');";
+
+                echo "$('input[name=\'correctAnswer\']').hide();";
+                // echo  "$('.jokerBtnDiv').prop('disabled', true);";
+                // echo "$('input[name=\'correctAnswer\']').prop('disabled', true);";
             }
             ?>
+
             $('button[name^="joker"]').on('click', function() {
                 $('.jokerBtnDiv').prop('disabled', true);
                 $('input[type="radio"]').prop('disabled', false);
@@ -249,7 +298,7 @@ require "connection.php";
 
                 // Set the text content of the new p element
                 newParagraphElement.text(<?php
-                                            echo json_encode($randomQuestionObj['description']);
+                                            echo json_encode(isset($randomQuestionObj['description']) ? $randomQuestionObj['description'] : null);
                                             ?>);
 
                 // Replace the h5 element with the new p element
